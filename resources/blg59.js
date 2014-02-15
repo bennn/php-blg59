a0c337cc5fbe3f14e74163e5ea272ed450cf4edb
Ben <ben@rentenna.com>
2014-02-15 04:31:39 -0500

    Initial files

---
+++ resources/blg59.js
@@ -0,0 +1,4 @@
+function nils () {
+    return "nils";
+};
+
---
+++ sync.sh
@@ -0,0 +1,4 @@
+# Log in to secure server, update files, exit
+# TODO log in, log out
+# TODO ignore *~, \#* files
+cp -r /Users/ben/code/php-blg59/* /Volumes/winhome/MyWeb/
