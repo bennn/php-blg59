2a098ff49e26fa91170e14e54ab873feb8c439b2
Ben Greenman <benjaminlgreenman@gmail.com>
2023-05-21 09:34:22 -0400

    gtp-checkup: inline run cmd, gotta use custom raco

--- resources/gtp-checkup-cloudlab.sh
+++ resources/gtp-checkup-cloudlab.sh
@@ -5,4 +5,9 @@ cd racket-8.8/src/build
 ../configure && make && make install
 cd ../../..
 ./racket-8.8/bin/raco pkg install --auto --clone gtp-checkup
-make all >& output.txt
+cd gtp-checkup
+RACO="../racket-8.8/bin/raco"
+MAIN=main.rkt
+PLT_TR_NO_CONTRACT_OPTIMIZE=1 ${RACO} make ${MAIN} >& output.txt
+PLT_TR_NO_CONTRACT_OPTIMIZE=1 PLTSTDERR="error info@gtp-checkup" ${RACO} test ${MAIN} >& output.txt
+cd ..
